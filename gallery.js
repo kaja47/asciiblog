a415585871df87de6defb01375fc09e48339a382
kaja47 <kaja47@k47.cz>
2017-09-11 22:59:05 +0200

    gallery.js - close button

--- gallery.js
+++ gallery.js
@@ -2,12 +2,15 @@ let box = document.createElement('div');
 box.style = 'display:none; position:fixed; top:0px; left:0px; background-color:rgba(0,0,0,0.7); width:100%; height:100%';
 let img = document.createElement('img');
 img.style = 'display:block; margin:auto; max-width:100%; max-height:100%';
+let x = document.createElement('div');
+x.style = 'position:fixed;top:0px;right:0px;font-size:1.5em;padding:0.5em;background:rgba(255,255,255,0.7);';
+x.textContent = "X";
 box.appendChild(img);
+box.appendChild(x);
 box.addEventListener('click', function(e) { hide(); });
 img.addEventListener('click', function(e) { move(+1); e.stopPropagation(); });
 
-let imgs = null;
-let active = null;
+let imgs = null, active = null;
 
 function move(n) { show(active + n); }
 function hide() { box.style.display = "none"; }
