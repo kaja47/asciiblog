974d31456808dd192632fc581abd9dc0c7d4053c
kaja47 <kaja47@k47.cz>
2017-11-25 22:46:26 +0100

    upload - copy images first, then html files and index.html and rss.xml last

--- upload.scala
+++ upload.scala
@@ -1,3 +1,5 @@
+package asciiblog
+
 import java.io.File
 import java.nio.file.StandardCopyOption._
 import java.nio.file.{ Files, NoSuchFileException }
@@ -11,6 +13,24 @@ if (args.length != 2) {
 
 val source = new File(args(0))
 val target = new File(args(1))
+
+
+// copy images
+
+val thumbSource = new File(source, "t")
+val thumbTarget = new File(target, "t")
+thumbTarget.mkdirs()
+
+for (s <- thumbSource.listFiles) {
+  val t = new File(thumbTarget, s.getName)
+  if (!t.exists) {
+    println(s"copying $s -> $t")
+    Files.copy(s.toPath, t.toPath, REPLACE_EXISTING)
+  }
+}
+
+
+// copy html files
 
 def parseFileIndex(lines: Iterator[String]): Map[String, String] =
   lines.map { l => val Array(h, f) = l.split(" ", 2) ; (f, h) }.toMap
@@ -41,29 +61,15 @@ def move(s: File, t: File, retries: Int): Unit = {
 }
 
 for {
-  (fn, shash) <- sourceIndex.toSeq.sortBy(_._1)
+  (fn, _) <- sourceIndex.toSeq.sortBy { case (f, _) => if (f == "index.html" || f == "rss.xml") (1, f) else (0, f) }
 } {
-
   val s = new File(source, fn)
   val t = new File(target, fn)
   if (!t.exists() || !targetIndex.contains(fn) || targetIndex(fn) != sourceIndex(fn)) {
     move(s, t, 5)
   }
-
 }
 
 Files.copy(sf.toPath, tf.toPath, REPLACE_EXISTING)
-
-val thumbSource = new File(source, "t")
-val thumbTarget = new File(target, "t")
-thumbTarget.mkdirs()
-
-for (s <- thumbSource.listFiles) {
-  val t = new File(thumbTarget, s.getName)
-  if (!t.exists) {
-    println(s"copying $s -> $t")
-    Files.copy(s.toPath, t.toPath, REPLACE_EXISTING)
-  }
-}
 
 }
